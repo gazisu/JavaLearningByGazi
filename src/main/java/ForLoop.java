f5c40d6a7974d42d3d7031e5d7bbe43a14a0890e
Shohel <Shohel@cel.local>
2023-04-02 15:29:35 +0600

    Forloop

---
+++ src/main/java/ForLoop.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class ForLoop {
+    public static void main(String[] args) {
+
+
+        for ( int star = 1; star <= 10; star++ ){
+            System.out.println(star+" * "+star);
+        }
+    }
+}
