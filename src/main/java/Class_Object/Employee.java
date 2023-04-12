752ac54a0e045d78789505ee85f2ef1c03236afd
gazisu <info.gazisu@gmail.com>
2023-04-12 22:58:50 +0600

    OOP concept learning with class & object

---
+++ src/main/java/Class_Object/Employee.java
@@ -0,0 +1,19 @@
+package Class_Object;
+
+import java.text.DateFormat;
+import java.text.FieldPosition;
+import java.text.ParsePosition;
+import java.util.Date;
+
+public class Employee {
+    String Name, Phone_Number, Address;
+    int ID;
+    String DOB;
+    String DOJ;
+
+    }
+
+
+
+
+
---
+++ src/main/java/Class_Object/EmployeeInformamtion.java
@@ -0,0 +1,22 @@
+package Class_Object;
+
+import javax.print.DocPrintJob;
+
+public class EmployeeInformamtion {
+    public static void main(String[] args) {
+
+        Employee emp1= new Employee();
+        emp1.ID=1001;
+        emp1.Name = "Sakib";
+        emp1.Address="Dhaka";
+        emp1.DOB= "1 April 1999";
+        emp1.DOJ="1 January 2022";
+
+        System.out.println("ID "+emp1.ID);
+        System.out.println("Name "+ emp1.Name);
+        System.out.println("Address "+emp1.Address);
+        System.out.println("DOB " + emp1.DOB);
+        System.out.println("DOJ "+ emp1.DOJ);
+
+    }
+}
